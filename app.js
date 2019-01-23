781e93bae9bc7b8254b9117498e5a3096b3c9dbb
kanavbhatia <kanavbhatia97@gmail.com>
2019-01-23 17:34:00 +0530

    main bar added

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let input = $('#inp')
 let result = $('#res')
+let result1 = $('#res1')
+let result2 = $('#res2')
 let button = $('#but')
 let key = 'a9654f11b8da4a98967183035192201'
 let icon = $('#icon')
@@ -12,12 +14,20 @@ button.click(function(){
         method: 'GET',
         success: function(data){
             info = `Temperature in ${data.location.region} (${data.location.country}) is ${data.current.temp_c} and it will be ${data.current.condition.text} today `
-            result.text(info)
+            var prop_temp = `${data.current.temp_c} C`
+            result.text(prop_temp)
+            result1.text(data.location.region)
+            result2.text(data.current.condition.text)
             console.log(info)
             var imgsource = `http://${data.current.condition.icon}`
             icon.attr('src',imgsource)
 
+        },
+        error: function(error){
+            result.text(error.responseText)
+            console.log(error)
         }
+        
     })
 
 })
