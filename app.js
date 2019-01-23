2657bdf8e545a4cce380078882dc44de2efdb27a
kanavbhatia <kanavbhatia97@gmail.com>
2019-01-23 12:19:52 +0530

    weather-icon

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// HTTPS: https://api.apixu.com/v1/current.json?key=a9654f11b8da4a98967183035192201&q=Paris
 let input = $('#inp')
 let result = $('#res')
 let button = $('#but')
 let key = 'a9654f11b8da4a98967183035192201'
+let icon = $('#icon')
 button.click(function(){
     let city = input.val()
     // console.log(value)
@@ -14,6 +14,9 @@ button.click(function(){
             info = `Temperature in ${data.location.region} (${data.location.country}) is ${data.current.temp_c} and it will be ${data.current.condition.text} today `
             result.text(info)
             console.log(info)
+            var imgsource = `http://${data.current.condition.icon}`
+            icon.attr('src',imgsource)
+
         }
     })
 
